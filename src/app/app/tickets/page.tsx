a466a98f96725219fd38a43b2eed1074589d39be
Frooszy <thecryzin@gmail.com>
2024-01-22 15:20:58 -0300

    Resolver on login page and ticket visuals changes

--- src/app/app/tickets/page.tsx
+++ src/app/app/tickets/page.tsx
@@ -1,10 +1,11 @@
 "use client"
 import { TicketService } from "@/services/api/ticket.service";
-import { Box, Button, Container, CssBaseline, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, FormControl, InputLabel, MenuItem, Paper, Select, TextField, Typography, useForkRef } from "@mui/material";
+import { Box, Button, CircularProgress, Container, CssBaseline, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, FormControl, InputLabel, MenuItem, Paper, Select, TextField, Typography, useForkRef } from "@mui/material";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
 
 const columns: GridColDef[] = [
   {
@@ -82,7 +83,9 @@ export default function SystemTickets() {
       setLoading(true)
       const ticket = await TicketService.create(data.title, data.description)
       router.push(`/app/tickets/${ticket.id}`)
+      toast.success('Ticket created successfully!')
     } catch (e) {
+      toast.error('Something went wrong!')
       console.error(e)
     } finally {
       setLoading(false)
@@ -95,49 +98,58 @@ export default function SystemTickets() {
       <Paper elevation={3} sx={{ display: 'flex', flexDirection: 'column', gap: 4, padding: 5 }}>
         <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
           <Typography variant="h4">Tickets</Typography>
-          <Button variant="outlined" size="large" onClick={handleClickOpen}>Create</Button>
+          {total ? <Button variant="outlined" size="large" onClick={handleClickOpen}>Create</Button> : null}
         </Box>
-        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 4 }}>
-          <Box sx={{ display: 'flex', gap: 2 }}>
-            <TextField type="search" placeholder="Search" fullWidth />
-            <FormControl sx={{ width: '20%' }} >
-              <InputLabel>Status</InputLabel>
-              <Select label="Status" defaultValue={1}>
-                <MenuItem value={1}>All</MenuItem>
-                <MenuItem value={2}>Pending</MenuItem>
-                <MenuItem value={3}>Resolved</MenuItem>
-              </Select>
-            </FormControl>
+        {total ?
+          <Box sx={{ display: 'flex', flexDirection: 'column', gap: 4 }}>
+            <Box sx={{ display: 'flex', gap: 2 }}>
+              <TextField type="search" placeholder="Search" fullWidth />
+              <FormControl sx={{ width: '20%' }} >
+                <InputLabel>Status</InputLabel>
+                <Select label="Status" defaultValue={1}>
+                  <MenuItem value={1}>All</MenuItem>
+                  <MenuItem value={2}>Pending</MenuItem>
+                  <MenuItem value={3}>Resolved</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
+            <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+              <DataGrid
+                rows={tickets}
+                loading={loading}
+                columns={columns}
+                paginationMode="server"
+                pageSizeOptions={[5, 10, 15, 20, 25]}
+                paginationModel={pagination}
+                onPaginationModelChange={setPagination}
+                rowCount={total}
+                initialState={{
+                  columns: {
+                    columnVisibilityModel: {
+                      user: false
+                    }
+                  }
+                }}
+              />
+            </Box>
+          </Box> :
+          <Box sx={{display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
+            <Typography variant="h4">You do not have any tickets at the moment</Typography>
+            <Typography variant="overline">You can create a ticket for our team to resolve your issue!</Typography>
+            <Button variant="outlined" size="large" onClick={handleClickOpen} sx={{mt: 2}}>Create ticket</Button>
           </Box>
-          <DataGrid
-            rows={tickets}
-            loading={loading}
-            columns={columns}
-            paginationMode="server"
-            pageSizeOptions={[5, 10, 15, 20, 25]}
-            paginationModel={pagination}
-            onPaginationModelChange={setPagination}
-            rowCount={total}
-            initialState={{
-              columns: {
-                columnVisibilityModel: {
-                  user: false
-                }
-              }
-            }}
-          />
-        </Box>
+        }
       </Paper>
       <Dialog open={open} onClose={handleClose} component='form' onSubmit={handleSubmit(onSubmit)}>
         <DialogTitle>Ticket</DialogTitle>
         <DialogContent>
           <DialogContentText>Enter some information below to have your ticket opened.</DialogContentText>
           <TextField autoFocus required label="Title" margin="dense" variant="filled" fullWidth {...register("title")} />
-          <TextField autoFocus required label="Description" margin="dense" variant="filled" fullWidth multiline rows={3} {...register("description")} disabled={loading} />
+          <TextField autoFocus required label="Description" margin="dense" variant="filled" fullWidth multiline rows={3} {...register("description")} />
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Cancel</Button>
-          <Button type="submit">Create</Button>
+          <Button type="submit" disabled={loading}>{loading ? <CircularProgress size={20} /> : "Create"}</Button>
         </DialogActions>
       </Dialog>
     </Container>
--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 import AuthContext from "@/contexts/auth";
+import yup from "@/helpers/validation";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { Box, Button, CircularProgress, Container, CssBaseline, Link, Paper, TextField, Typography } from "@mui/material";
 import { useContext, useState } from "react";
 import { useForm } from "react-hook-form";
@@ -10,10 +12,16 @@ type Data = {
   password: string
 }
 
+const schema = yup.object({
+  email: yup.string().email().required(),
+  password: yup.string().min(8).required()
+})
+
 export default function AuthLogin() {
   const { login } = useContext(AuthContext)
 
-  const { handleSubmit, register } = useForm<Data>({
+  const { handleSubmit, register, formState: { errors } } = useForm<Data>({
+    resolver: yupResolver(schema),
     defaultValues: {
       email: "",
       password: ""
@@ -47,14 +55,14 @@ export default function AuthLogin() {
             <Typography variant='h5' fontWeight='bold'>Sign In</Typography>
           </Box>
           <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, width: '100%' }}>
-            <TextField required label='Email' {...register("email")} />
-            <TextField required type='password' label='Password' {...register("password")} />
+            <TextField label='Email' error={!!errors.email} helperText={errors.email?.message} {...register("email")} />
+            <TextField type='password' error={!!errors.password} helperText={errors.password?.message} label='Password' {...register("password")} />
           </Box>
           <Box sx={{ display: 'flex', justifyContent: 'end', width: '100%', mt: 0.8, mb: 3 }}>
             <Link href='#' variant='subtitle2' underline='none'>Forgot password?</Link>
           </Box>
           <Box sx={{ display: 'flex', width: '100%', mb: 2 }}>
-            <Button type="submit" variant='outlined' disabled={loading} sx={{ width: '100%', height: "56px"}}>{loading ? <CircularProgress size={20} /> : "Login"}</Button>
+            <Button type="submit" variant='outlined' disabled={loading} sx={{ width: '100%', height: "56px" }}>{loading ? <CircularProgress size={20} /> : "Login"}</Button>
           </Box>
           <Link href='/auth/register' variant='subtitle2' underline='none'>Create Account</Link>
         </Paper>
